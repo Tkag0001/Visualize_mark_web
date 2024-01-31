7255190f66401ba311cc42788acaacd205011504
Trinh Huynh Thinh Khang <trinhhuynhthinhkhang@gmail.com>
2024-01-31 09:04:48 +0700

    build: move server.js into app.js

--- app.js
+++ app.js
@@ -1,27 +1,37 @@
 const express = require('express')
 let app = express();
 const morgan = require('morgan')
+const dotenv = require('dotenv')
+dotenv.config({path: './config.env'})
+const mongoose = require('mongoose')
 
 //Connect router
 const classRouter = require('./Routes/classRouter')
-
-//Custom middleware
-// const logger = function (req, res, next) {
-//     console.log('Custom middleware called')
-//     next();
-// }
+mongoose.connect(process.env.CONN_STR, {
+    useNewUrlparser: true
+}).then((conn) => {
+    // console.log(conn)
+    console.log('DB Connection Successful')
+}).catch((error) =>{
+    console.log('Some error has the occured')
+})
 
 app.use(express.json())
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
+
 app.use(express.static('./Public'))
-// app.use(logger)
 app.use((req, res, next) => {
     req.requestedAt = new Date().toISOString()
     next()
 })
 
 app.use('/api/v1/classes', classRouter)
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('server has started')
+})
 
 module.exports = app
