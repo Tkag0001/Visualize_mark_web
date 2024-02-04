80f44eecc8f8a4cf122b2b8e76e9669077f97609
Trinh Huynh Thinh Khang <trinhhuynhthinhkhang@gmail.com>
2024-02-04 09:58:27 +0700

    build: add upload database button

--- Public/Templates/script/create.js
+++ Public/Templates/script/create.js
@@ -1,64 +1,3 @@
-// const XLSX = require('XLSX')
-// const form = $('form')
-// fileInput = $(".file-input")
-// progressArea = $(".progress-area")
-// uploadedArea = $(".uploaded-area")
-// console.log(form)
-
-// fileInput.onchange = ({target})=>{
-//   let file = target.files[0];
-//   if(file){
-//     let fileName = file.name;
-//     if(fileName.length >= 12){
-//       let splitName = fileName.split('.');
-//       fileName = splitName[0].substring(0, 13) + "... ." + splitName[1];
-//     }
-//     uploadFile(fileName);
-//   }
-// }
-
-// function uploadFile(name){
-//   let xhr = new XMLHttpRequest();
-//   xhr.open("POST", "php/upload.php");
-//   xhr.upload.addEventListener("progress", ({loaded, total}) =>{
-//     let fileLoaded = Math.floor((loaded / total) * 100);
-//     let fileTotal = Math.floor(total / 1000);
-//     let fileSize;
-//     (fileTotal < 1024) ? fileSize = fileTotal + " KB" : fileSize = (loaded / (1024*1024)).toFixed(2) + " MB";
-//     let progressHTML = `<li class="row">
-//                           <i class="fas fa-file-alt"></i>
-//                           <div class="content">
-//                             <div class="details">
-//                               <span class="name">${name} • Uploading</span>
-//                               <span class="percent">${fileLoaded}%</span>
-//                             </div>
-//                             <div class="progress-bar">
-//                               <div class="progress" style="width: ${fileLoaded}%"></div>
-//                             </div>
-//                           </div>
-//                         </li>`;
-//     uploadedArea.addClass("onprogress");
-//     progressArea.innerHTML = progressHTML;
-//     if(loaded == total){
-//       progressArea.innerHTML = "";
-//       let uploadedHTML = `<li class="row">
-//                             <div class="content upload">
-//                               <i class="fas fa-file-alt"></i>
-//                               <div class="details">
-//                                 <span class="name">${name} • Uploaded</span>
-//                                 <span class="size">${fileSize}</span>
-//                               </div>
-//                             </div>
-//                             <i class="fas fa-check"></i>
-//                           </li>`;
-//       uploadedArea.removeClass("onprogress");
-//       uploadedArea.insertAdjacentHTML("afterbegin", uploadedHTML);
-//     }
-//   });
-//   let data = new FormData(form);
-//   xhr.send(data);
-// }
-
 function uploadFile(data, table) {
     let content = "";
     for (let el of data) {
@@ -77,22 +16,39 @@ function uploadFile(data, table) {
         if (temp.indexOf('undefined') !== -1) {
             $('.wrapper_table').css("display", "none");
             alert('Can\'t read undefined');
-            return;  // This will exit the function
+            return false;  // This will exit the function
         } else {
             content += temp;
         }
     }
     table.html(content);
     $('.wrapper_table').css("display", "inline-block");
+    return true
 }
 
+function uploadDb(data) {
+    console.log(data)
+    fetch("https://odd-ruby-trout-cap.cyclic.app/api/v1/classes/?", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+        .then(response => response.json())
+        .then(data => console.log('Success:', data))
+        .catch(err => {
+            console.log(err)
+            alert('Upload is false!')
+        });
+}
 
 $(document).ready(function () {
     const form = $('form')
     const fileInput = $(".file-input")
     const tbody = $('tbody')
-    const progressArea = $(".progress-area")
-    const uploadedArea = $(".uploaded-area")
+    var listObjects = []
+    var uploadF = false
 
     console.log(form)
     console.log(tbody)
@@ -108,21 +64,6 @@ $(document).ready(function () {
 
 
     fileInput.click(function () { console.log('click input') })
-    // fileInput.change((event) => {
-    //     const file = event.target.files[0];
-    //     const reader = new FileReader();
-    //     reader.onload = (event) => {
-    //         const data = event.target.result;
-    //         const workbook = XLSX.read(data, { type: 'array' });
-    //         const sheetName = workbook.SheetNames[0];
-    //         const sheet = workbook.Sheets[sheetName];
-    //         const listObject = XLSX.utils.sheet_to_json(sheet);
-    //         console.log(listObject);
-    //     };
-    //     reader.readAsArrayBuffer(file);
-    // });
-    let listObjects = []
-
     fileInput.change(function (event) {
         const file = event.target.files[0];
         const reader = new FileReader();
@@ -140,14 +81,46 @@ $(document).ready(function () {
                 }
                 return item;
             });
-            listObjects.push(objects)
+            listObjects = objects
 
-            uploadFile(listObjects[listObjects.length - 1], tbody)
+            uploadF = uploadFile(listObjects, tbody)
         };
         reader.readAsArrayBuffer(file);
-
-        console.log(listObjects);
-
-
     });
+
+    $('#pushDb').click(function (event) {
+        // console.log('push')
+        if (!uploadF) {
+            alert('Data is invalid!')
+        }
+        else {
+            const result = confirm('Are you sure you want to upload data?')
+            if (result == false) {
+                console.log('cancel upload')
+                event.preventDefault()
+            }
+            else {
+                console.log(listObjects)
+                console.log('upload database')
+                uploadDb(listObjects)
+            }
+        }
+
+    })
+
 })
+
+const data = {
+    // your JSON data here
+};
+
+// fetch('http://your-server-url/', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//     },
+//     body: JSON.stringify(data),
+// })
+// .then(response => response.json())
+// .then(data => console.log('Success:', data))
+// .catch((error) => console.error('Error:', error));
