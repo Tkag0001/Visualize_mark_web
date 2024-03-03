cf72a8d8901b1d846fa719211bb55bf81310bf0f
Trinh Huynh Thinh Khang <trinhhuynhthinhkhang@gmail.com>
2024-03-03 07:57:42 +0700

    build: change location

--- Routes/replaceHTML.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(template, listMarks, dataBarChart, dataPieChart) {
-    let output = template.replace('{{%dataBarChart%}}', "[" + dataBarChart + "]");
-    output = output.replace('{{%dataPieChart%}}', "[" + dataPieChart + "]")
-
-    output = output.replace('{{%CONTENT%}}', `Phổ điểm môn: ${listMarks['TenMH']}<br>Lớp: ${listMarks['MaLop']}<br>Khoa: ${listMarks['Khoa']}<br>Học kì: ${listMarks['HocKy']}<br>Giảng viên: ${listMarks['HoTenGV']}`)
-    return output
-}
